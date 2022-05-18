c8ce9ef29723912d7cb6f023c1302188186e3bb3
matheusllourenco <matheuslimalourenco@gmail.com>
2022-05-17 21:48:57 -0300

    adicionando novos usuarios e idUsuarios

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-const { request } = require('express');
 const express = require ('express');
 
 const app = express();
@@ -12,5 +11,23 @@ app.get('/usuarios', (request, response) => {
     response.json(usuarios)
 });
 
+app.post('/usuarios', (request, response) => {
+    const novoUssuario = {
+        nome: 'joão Pedro',
+        idade: 22
+    };
 
-app.listen(3000);
+    usuarios.push(novoUssuario);
+
+    response.json(novoUssuario);
+});
+
+app.put('/usuarios/:idUsuarios', (request, response) => {
+    const idUsuarios = request.params.idUsuarios
+    response.json(usuarios)
+});
+
+
+app.listen(3000, () => {
+    console.log('minha aplicação on');
+});
