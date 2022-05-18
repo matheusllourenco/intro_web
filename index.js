38b182a3855917531775d718b4dcccc1d94fa0a6
matheusllourenco <matheuslimalourenco@gmail.com>
2022-05-17 21:28:43 -0300

    adicionando usuario

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-console.log("olasa");
+const { request } = require('express');
+const express = require ('express');
+
+const app = express();
+
+const usuarios = [{
+    nome: 'matheus',
+    idade: '25'
+}];
+
+app.get('/usuarios', (request, response) => {
+    response.json(usuarios)
+});
+
+
+app.listen(3000);
